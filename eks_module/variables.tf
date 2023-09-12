adc1614f8cd71977cde48f87a57bed483fd01d5a
aayushm96 <aayushm96@yahoo.com>
2023-09-12 19:15:50 +0530

    Update variables.tf

--- eks_module/variables.tf
+++ eks_module/variables.tf
@@ -7,11 +7,10 @@ variable "cidr" {}
 
 variable "public-subnet-1" {}
 variable "public-subnet-2" {}
-variable "public-subnet-3" {}
+
 
 variable "private-subnet-1" {}
 variable "private-subnet-2" {}
-variable "private-subnet-3" {}
 
 variable "instance_type" {}
 variable "instance_disk_size" {}
